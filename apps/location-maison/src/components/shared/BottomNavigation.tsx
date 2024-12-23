ad6c96400d5cb2d954a6fd609070d64c60d79c4a
RitchiGlenn <glenneriss@gmail.com>
2024-12-23 22:47:19 +0000

    fix responsivity screens

--- apps/location-maison/src/components/shared/BottomNavigation.tsx
+++ apps/location-maison/src/components/shared/BottomNavigation.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { routes } from '@/constantes/routes';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { useCurrentUser } from '@/hooks/use-current-user';
 
 const menu = [
     {
@@ -30,8 +31,12 @@ const menu = [
 ]
 export const BottomNavigation: React.FC = () => {
     const pathnames = usePathname()
+    const user = useCurrentUser()
+    if(!user){
+        return null
+    }
     return (
-        <div className='fixed bottom-0 z-50 w-full flex bg-white p-2 justify-between px-4 md:hidden'>
+        <div className='fixed bottom-0 z-50 w-full flex bg-white p-2 justify-between px-4 md:hidden shadow'>
             {
                 menu.map((item, key) => (
                     <Link href={item.link} key={key} className={clsx({
