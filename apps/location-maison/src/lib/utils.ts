d1aea89772138797c11397bf79219b55c6b90037
YourispenDy <rolyspen1997@gmail.com>
2025-03-19 06:48:53 +0000

    fix: Changer l’emplacement de l’icone du user sur la home page

--- apps/location-maison/src/lib/utils.ts
+++ apps/location-maison/src/lib/utils.ts
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-export const TypeProperty:any = {
+export const TypeProperty: Record<string, string> = {
   House: "Maison",
   Apartment: "Appartement",
   Studio: "Studio",
@@ -12,6 +12,7 @@ export const TypeProperty:any = {
   Kiosk: "Kiosque",
   Shop: "Magasin",
   Office: "Bureau",
-  Building:"Immeuble",
-  //Villa:"Villa"
+  Building: "Immeuble",
+  // Villa: "Villa"
 };
+
