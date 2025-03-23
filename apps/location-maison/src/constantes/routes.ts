37e97cdb6cb207d7675b822f40fd92a78a0ac419
Glenn Eriss <glenneriss@gmail.com>
2025-03-23 01:14:08 +0000

    feat data deletion page

--- apps/location-maison/src/constantes/routes.ts
+++ apps/location-maison/src/constantes/routes.ts
@@ -30,5 +30,6 @@ export const routes = {
         reset_password: '/password-reset',
         confidentiality: '/privacy-policy',
         terms_of_use: '/terms-of-use',
+        data_deletion: '/data-deletion'
     }
 }
