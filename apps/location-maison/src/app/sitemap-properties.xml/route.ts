d8998e57dc6c3e21ebfd9cdf69be62be3ac97897
Glenn Eriss <glenneriss@gmail.com>
2025-04-15 17:17:28 +0000

    refactor sitemap

---
+++ apps/location-maison/src/app/sitemap-properties.xml/route.ts
@@ -0,0 +1,26 @@
+import { routes } from '@/constantes/routes'
+
+export async function GET() {
+  const baseUrl = 'https://location-maison-gabon.vercel.app'
+
+  const urls = Object.values(routes.protected)
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls
+    .map(
+      (path) => `
+    <url>
+      <loc>${baseUrl}${path}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>`
+    )
+    .join('')}
+</urlset>`
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+}
