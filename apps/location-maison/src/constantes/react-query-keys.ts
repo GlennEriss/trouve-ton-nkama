acfd4e8f6f73026cb0097de8f98742c196253bb4
Glenn Eriss <glenneriss@gmail.com>
2025-04-15 11:16:11 +0000

    feat refactor homepage

--- apps/location-maison/src/constantes/react-query-keys.ts
+++ apps/location-maison/src/constantes/react-query-keys.ts
@@ -1,5 +1,6 @@
 const queryKeys = {
     properties: 'properties',
+    propertie_carousel: 'carousel_properties',
     favoris: 'favoris',
 } as const
 
---
+++ apps/location-maison/src/mocks/mocksAds.ts
@@ -0,0 +1,18 @@
+export const ads = [
+    {
+      id: 1,
+      image: "https://www.touchinnovative.com/img/blg/6-170223162621.jpg",
+      link: "#",
+    },
+    {
+      id: 2,
+      image:
+        "https://static.wixstatic.com/media/a533e7_c9ab2af50964476eb4c3f33ce31db6ce~mv2.jpg/v1/fill/w_618,h_388,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/a533e7_c9ab2af50964476eb4c3f33ce31db6ce~mv2.jpg",
+      link: "#",
+    },
+    {
+      id: 3,
+      image: "https://www.touchinnovative.com/img/blg/6-170223162621.jpg",
+      link: "#",
+    },
+  ];
