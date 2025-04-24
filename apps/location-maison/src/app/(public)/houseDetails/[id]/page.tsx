8b1d29156afec4a2de17850225b029dc6bada5de
Glenn Eriss <glenneriss@gmail.com>
2025-04-24 02:43:48 +0000

    feat facebook share image

--- apps/location-maison/src/app/(public)/houseDetails/[id]/page.tsx
+++ apps/location-maison/src/app/(public)/houseDetails/[id]/page.tsx
@@ -2,7 +2,6 @@ import PreviewProperty from '@/components/preview-property/PreviewProperty'
 import { getPropertyById } from '@/db/property.db'
 import { notFound } from 'next/navigation'
 import React from 'react'
-import Head from 'next/head'
 
 export default async function page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
@@ -12,34 +11,8 @@ export default async function page({ params }: { params: Promise<{ id: string }>
     return notFound()
   }
 
-  const baseUrl = 'https://location-maison-gabon.vercel.app'
-  const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "Product",
-    "name": property.title,
-    "image": property.images?.[0]?.fileURL || "/home.png",
-    "description": property.description,
-    "brand": {
-      "@type": "Brand",
-      "name": "LogisGabon"
-    },
-    "offers": {
-      "@type": "Offer",
-      "priceCurrency": "XAF",
-      "price": property.price,
-      "availability": "https://schema.org/InStock",
-      "url": `${baseUrl}/houseDetails/${property.id}`
-    }
-  }
-
   return (
     <div className='md:px-10 lg:px-20'>
-      <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />
-      </Head>
       <PreviewProperty property={property} />
     </div>
   )
