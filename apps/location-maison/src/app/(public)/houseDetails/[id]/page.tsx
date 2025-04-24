c4a7aa758f067072a864d51aed593131b241e370
Glenn Eriss <glenneriss@gmail.com>
2025-04-24 03:22:32 +0000

    refactor og for meta

--- apps/location-maison/src/app/(public)/houseDetails/[id]/page.tsx
+++ apps/location-maison/src/app/(public)/houseDetails/[id]/page.tsx
@@ -3,6 +3,34 @@ import { getPropertyById } from '@/db/property.db'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
+export async function generateMetadata({ params }: { params:  Promise<{ id: string }> }) {
+  const { id } = await params
+  const property = await getPropertyById(id)
+
+  if (!property) {
+    return {}
+  }
+
+  return {
+    title: property.title,
+    description: property.description,
+    openGraph: {
+      title: property.title,
+      description: property.description,
+      images: [
+        {
+          url: property.images[0]?.fileURL || '',
+          width: 1200,
+          height: 630,
+          alt: property.title,
+        },
+      ],
+      url: `https://www.logi-market.com/houseDetails/${property.id}`,
+      type: 'website',
+    },
+  }
+}
+
 export default async function page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
   const property = await getPropertyById(id)
