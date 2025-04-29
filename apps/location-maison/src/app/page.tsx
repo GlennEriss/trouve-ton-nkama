13bc64bd6cb7132151241377c201a9239141aae7
Glenn Eriss <glenneriss@gmail.com>
2025-04-29 11:08:53 +0000

    refactor delete script google ads

--- apps/location-maison/src/app/page.tsx
+++ apps/location-maison/src/app/page.tsx
@@ -8,10 +8,6 @@ export default async function Home() {
     const session = await auth()
     return (
         <div className="bg-gray-100 dark:bg-gray-900">
-            <head>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2799688336707362"
-                    crossOrigin="anonymous"></script>
-            </head>
             <Navbar session={session} />
             <HomePage />
         </div>)
