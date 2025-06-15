0df1c20d755a0ee6d3e9cbcc9d55f07c655fb578
Glenn Eriss <glenneriss@gmail.com>
2025-06-15 14:17:14 +0000

    refactor color featured section

--- apps/location-maison/src/components/home-page/FeaturedSection.tsx
+++ apps/location-maison/src/components/home-page/FeaturedSection.tsx
@@ -16,7 +16,7 @@ export default function FeaturedSection() {
    }
 
   return (
-    <section className='relative rounded-3xl space-y-3 bg-gradient-to-r from-yellow-50 to-amber-50 dark:from-yellow-900/10 dark:to-amber-900/10 p-5 py-10 overflow-hidden'>
+    <section className='relative rounded-3xl space-y-3 bg-gradient-to-r from-[#C1DEE8] to-[#FBD9B9] dark:from-[#146B67]/10 dark:to-[#1FA89B]/10 p-5 py-10 overflow-hidden'>
       {/* Image d'arrière-plan */}
       <div className="absolute inset-0 z-0">
         <Image
