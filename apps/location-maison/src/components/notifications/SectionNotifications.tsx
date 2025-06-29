588b2235253c7f304a47ebbb4298b759ad8efed9
Glenn Eriss <glenneriss@gmail.com>
2025-06-29 18:30:54 +0000

    fix build error

--- apps/location-maison/src/components/notifications/SectionNotifications.tsx
+++ apps/location-maison/src/components/notifications/SectionNotifications.tsx
@@ -80,14 +80,14 @@ export default function SectionNotifications() {
                   <Link
                     href={notification.actionUrl}
                     className="text-sm text-blue-600 dark:text-blue-400 hover:underline"
-                    onClick={() => markAsRead(notification.id)}
+                    onClick={() => markAsRead(notification.id ?? '')}
                   >
                     {notification.message}
                   </Link>
                 ) : (
                   <button
                     className="text-sm text-gray-800 dark:text-gray-300 text-left"
-                    onClick={() => markAsRead(notification.id)}
+                    onClick={() => markAsRead(notification.id ?? '')}
                   >
                     {notification.message}
                   </button>
