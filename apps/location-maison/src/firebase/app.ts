fed8d2bf00629a1bfd977c8b65e5572c1e48780d
YourispenDy <rolyspen1997@gmail.com>
2025-07-16 23:46:26 +0000

    fix: correct getApps usage in Firebase setup and add phone authentication exports

--- apps/location-maison/src/firebase/app.ts
+++ apps/location-maison/src/firebase/app.ts
@@ -2,7 +2,7 @@ import { firebaseClientConfig } from './firebaseClientConfig';
 import { initializeApp, getApp, getApps } from 'firebase/app';
 
 const setupFirebase = () => {
-  if (getApps.length) return getApp();
+  if (getApps().length) return getApp();
   return initializeApp(firebaseClientConfig);
 };
 
