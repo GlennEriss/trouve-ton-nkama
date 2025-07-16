81f93256bdb4f0b2135d522147d799b1c3a00fc5
YourispenDy <rolyspen1997@gmail.com>
2025-07-16 23:46:03 +0000

    feat: add support contact information and new routes for phone verification and profile completion

--- apps/location-maison/src/constantes/routes.ts
+++ apps/location-maison/src/constantes/routes.ts
@@ -24,11 +24,13 @@ export const routes = {
         notifications: '/settings',
         notification_list: '/list-notifications',
         my_balance: '/my-balance',
+        verify_phone: '/verify-phone',
     },
     public: {
         signinSignup: '/signin-signup',
         signin: '/signin',
         signup: '/signup',
+        completeProfile: '/complete-profile',
         homePage: '/',
         reset_password: '/password-reset',
         passwordResetRequest: '/request-password-reset',
@@ -40,5 +42,13 @@ export const routes = {
         data_deletion: '/data-deletion',
         search: '/search',
         search_property: '/search'
+    },
+    public_google: {
+        homePage: '/',
+        signin: '/signin',
+        signup: '/signup',
+        confidentiality: '/privacy-policy',
+        terms_of_use: '/terms-of-use',
+        search: '/search',
     }
 }
