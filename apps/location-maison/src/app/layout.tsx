dab7701bd06313a0f8b121c9069be3a447597809
YourispenDy <rolyspen1997@gmail.com>
2025-07-17 04:25:28 +0000

    feat: add additional Google site verification meta tags and update favicon links in layout component

--- apps/location-maison/src/app/layout.tsx
+++ apps/location-maison/src/app/layout.tsx
@@ -22,7 +22,11 @@ export default async function RootLayout({
       <head>
         <meta name="google-site-verification" content="owGLe__J-ZZiJvB-iZzlfxianxrwoO8vdRyxKFfSkTk" />
         <meta name="google-site-verification" content="RJWLkFimD2EEl5ocQ9ZmhdfjCg4CKWW19qwKFUSu8b8" />
+        <meta name="google-site-verification" content="srCtEUJLseTnipvVuamNBCo59HLskS-iUAGuSxvg2ek" />
         <meta name="google-adsense-account" content="ca-pub-2799688336707362" />
+        <link rel="icon" type="image/x-icon" href="/logo.ico" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
+        <link rel="apple-touch-icon" href="/logo.ico" />
       </head>
       <body
         className={cn('antialiased overscroll-y-none', inter.className)}
