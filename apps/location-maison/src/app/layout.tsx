d236e0b1bd5f04f541180c4ec3af03466ab3aba9
YourispenDy <rolyspen1997@gmail.com>
2025-07-17 09:32:04 +0000

    feat: add favicon.ico and update layout component to include new icon links and canonical URL for improved SEO

--- apps/location-maison/src/app/layout.tsx
+++ apps/location-maison/src/app/layout.tsx
@@ -21,17 +21,18 @@ export default async function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="google-site-verification" content="owGLe__J-ZZiJvB-iZzlfxianxrwoO8vdRyxKFfSkTk" />
-        <meta name="google-site-verification" content="RJWLkFimD2EEl5ocQ9ZmhdfjCg4CKWW19qwKFUSu8b8" />
-        <meta name="google-site-verification" content="srCtEUJLseTnipvVuamNBCo59HLskS-iUAGuSxvg2ek" />
+
         <meta name="google-adsense-account" content="ca-pub-2799688336707362" />
-        <link rel="canonical" href="https://www.tonnkama.com" />
-        <link rel="icon" type="image/x-icon" href="/logo.ico" />
-        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
-        <link rel="apple-touch-icon" href="/logo.ico" />
-        <link rel="shortcut icon" href="/logo.ico" />
-        <link rel="icon" sizes="16x16" href="/logo.ico" />
-        <link rel="icon" sizes="32x32" href="/logo.ico" />
+
+        <link rel="canonical" href="https://www.tonnkama.com/" />
+
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        <link rel="icon" type="image/svg+xml" href="/logo.svg"/>
+
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
       </head>
+
       <body
         className={cn('antialiased overscroll-y-none', inter.className)}
       >
